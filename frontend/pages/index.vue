88ca1d90e1833aea72c390d431b5eb87ee4bbca7
Matton <45633887+monsieurwai@users.noreply.github.com>
2020-09-19 01:18:35 +0200

    adding preview prismic

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>index</div>
+  <div></div>
 </template>
+
+<script>
+export default {
+  async asyncData({ $prismic, params, error }) {
+    const document = await $prismic.api.getByUID("accueil");
+
+    if (document) {
+      return { document };
+    } else {
+      error({ statusCode: 404, message: "Page not found" });
+    }
+  },
+};
+</script>
